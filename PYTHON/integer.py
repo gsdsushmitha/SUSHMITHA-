651cd70f45e845c7e4a2f51689cc26f71147ee0a
gsdsushmitha <68274832+gsdsushmitha@users.noreply.github.com>
2020-07-22 21:54:41 +0530

    Add files via upload

---
+++ PYTHON/integer.py
@@ -0,0 +1,13 @@
+num = -1
+  
+if num > 1:  
+   for i in range(2,num):  
+       if (num % i) == 0:  
+           print(num,"is not a prime number")  
+           print(i,"times",num//i,"is",num)  
+           break  
+   else:  
+       print(num,"is a prime number")  
+         
+else:  
+      print(num,"is neither prime nor composite")
