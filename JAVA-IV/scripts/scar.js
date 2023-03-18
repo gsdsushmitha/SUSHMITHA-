144098d5cd4a918db40b12b16f020cf8bcc264fd
gsdsushmitha <68274832+gsdsushmitha@users.noreply.github.com>
2023-03-18 09:59:17 +0530

    Delete JAVA-IV directory

--- JAVA-IV/scripts/scar.js
+++
@@ -1,9 +0,0 @@
-console.log("os version");{
-    var OSName = "Unknown OS";
-    if (navigator.userAgent.indexOf("Win") != -1) OSName = "Windows10";
-    if (navigator.userAgent.indexOf("Mac") != -1) OSName = "Macintosh8";
-    if (navigator.userAgent.indexOf("Linux") != -1) OSName = "Linux7";
-    if (navigator.userAgent.indexOf("Android") != -1) OSName = "Android9";
-    if (navigator.userAgent.indexOf("like Mac") != -1) OSName = "iOS6";
-    console.log(' OS name and version: ' + OSName);
-  }
